2c947c424f4195ce22fb2d703795a0aa4be3cce3
chaochao2016 <zju_zcc2016@163.com>
2017-04-10 16:39:12 +0800

    测试Git同步项目myFistProject

---
+++ myFirstGitProject.cpp
@@ -0,0 +1,9 @@
+//This is a test file to test git usage.
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"You are intelligent^-^"<<endl;
+	cout<<"2017-04-10,This is a nice day"<<endl;
+	return 0;
+}
